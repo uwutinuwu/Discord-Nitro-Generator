cd41662d90530822c11df0a0fbdb276f7d65b7d3
uwutinuwu <71038209+uwutinuwu@users.noreply.github.com>
2020-09-09 16:01:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random, string
 
 amount = int(input('Amount of nitro codes to generate: '))
 value = 1
-while value <= amount:
+while value <= amount:10
     code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
     f = open('Codes.txt', "a+")
     f.write(f'{code}\n')
